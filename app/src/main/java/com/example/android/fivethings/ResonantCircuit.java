9991f5ba4fafe82c65e36c38994b200d2af975c9
v-os <voidef@gmail.com>
2016-04-11 17:17:22 +0300

    Animated diagram added. Android Studio 2.0

--- app/src/main/java/com/example/android/fivethings/ResonantCircuit.java
+++ app/src/main/java/com/example/android/fivethings/ResonantCircuit.java
@@ -1,7 +1,9 @@
 package com.example.android.fivethings;
 
-import android.support.v7.app.AppCompatActivity;
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
 
 public class ResonantCircuit extends AppCompatActivity {
 
@@ -9,5 +11,20 @@ public class ResonantCircuit extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_resonant_circuit);
+
+        playAnimation();
+    }
+
+    void playAnimation() { // Load the ImageView that will host the animation and
+        // set its background to our AnimationDrawable XML resource.
+        ImageView img = (ImageView) findViewById(R.id.animation);
+//        img.setBackgroundResource(R.drawable.tuned_circuit_animation);
+
+        // Get the background, which has been compiled to an AnimationDrawable object.
+        AnimationDrawable frameAnimation = (AnimationDrawable) img.getBackground();
+
+        // Start the animation (looped playback by default).
+        frameAnimation.start();
     }
 }
+
